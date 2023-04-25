fef317c78f7a8aa20b22676e67ddbf4b074f746e
freidhofkyle <131631808+freidhofkyle@users.noreply.github.com>
2023-04-25 00:58:48 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# A Basic Python Program 
+
+
---
+++ Setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh 
+export pkg
+
+
+if ! which python3 >> /dev/null; then
+        echo "Python not Found"
+        exit
+
+else
+
+        for pkg in $("echo"); do continue ;done
+
+
+fi 
+
+sudo sh -c "cp Zelt /usr/bin"
+rm Zelt 
+
+
+    
+        
